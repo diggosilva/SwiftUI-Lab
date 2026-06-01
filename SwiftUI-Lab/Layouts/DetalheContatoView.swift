92d6267f8b0710474dfb59167b2acd206c306dbb
Diggo Silva <rdiggosilva@gmail.com>
2026-06-01 09:30:28 -0300

    feat(view): implementa layout e preview da tela de detalhes

---
+++ SwiftUI-Lab/Layouts/DetalheContatoView.swift
@@ -0,0 +1,30 @@
+//
+//  DetalheContatoView.swift
+//  SwiftUI-Lab
+//
+//  Created by Diggo Silva on 31/05/26.
+//
+
+import SwiftUI
+
+struct DetalheContatoView: View {
+    
+    let usuario: Usuario
+    
+    var body: some View {
+        VStack {
+            Image(systemName: usuario.fotoIcone)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+            
+            Text(usuario.nome)
+                .font(.headline)
+        }
+    }
+}
+
+#Preview {
+    DetalheContatoView(usuario: Usuario(nome: "Sonic The Hedgehog", username: "@sonichedgehog", fotoIcone: "spigot.fill"))
+}
