2598e9c5bd133162a9839fbaf8779583d0ba5a32
Diggo Silva <rdiggosilva@gmail.com>
2026-06-09 09:44:25 -0300

    Mock removido e identificado o array com o tipo Usuario

--- SwiftUI-Lab/Layouts/ListaContatosView.swift
+++ SwiftUI-Lab/Layouts/ListaContatosView.swift
@@ -11,12 +11,7 @@ struct ListaContatosView: View {
     private let userDefaults = UserDefaults.standard
     private let keyContatos = "MeusContatos"
     
-    @State private var contatos = [
-        Usuario(nome: "Mario Bros", username: "@mariobros", fotoIcone: "https://t2.tudocdn.net/680322?w=1920", favorito: false),
-        Usuario(nome: "Luigi Bros", username: "@luigibros", fotoIcone: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3uMtSWAztU-krcKEZ4i2WZSzm62Fj9_MIQ&s", favorito: false),
-        Usuario(nome: "Link Zelda", username: "@linkzelda", fotoIcone: "https://cdn.wikimg.net/en/zeldawiki/images/thumb/0/0a/LANS_Link_Render.png/320px-LANS_Link_Render.png", favorito: false),
-    ]
-    
+    @State private var contatos: [Usuario] = []
     @State private var exibirCadastro = false
     
     var body: some View {
